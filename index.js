5984b60fc6af361518aa31306547cab296bde053
John <jyk595@gmail.com>
2021-07-08 10:10:11 -0700

    commit tree ddd6025bf6435b660930766bb211cf8cc1326f42

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map((line) => {
+    const tokens = line.split(" ");
+    const newArray = [];
+
+    const capitalizedTokens = tokens.map((token) => {
+      newArray.push(token.charAt(0).toUpperCase() + token.slice(1));
+    });
+    const response = newArray.join(" ");
+    return response;
+  });
 }
+
+// Step 1: Separate each sentence by " " into a new array
+// Step 2: Target each array item and capitalize just the first letter in each array.
+// Step 3: Add a " " back to the end of each array, except for the last items
+// Step 4: Concatenate each nested array back together to become a full sentence again.
